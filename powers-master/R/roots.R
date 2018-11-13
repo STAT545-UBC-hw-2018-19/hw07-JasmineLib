f35c46157fb1ef06a02ae2b2de4ec5612b93ab12
JasmineLib <yamine7@gmail.com>
2018-11-13 11:09:03 -0800

    update hw07 first commit

---
+++ powers-master/R/roots.R
@@ -0,0 +1,19 @@
+#'#' Apply a useful root
+#'
+#'
+#'@param x the vector for which you want the root
+#'@param y the integer root that you want to take (ie square root = 2, cube root = 3, etc)
+#'@param plot_it Display a plot of \code{x} vs the output? Use logical.
+#' \code{FALSE} by default.
+#'
+#'@return A vector that is the:
+#' \itemize{
+#'      \item root (for \code{root})
+#'}
+#'  of \code{x}.
+#'
+#'
+#'@rdname roots
+#'@export
+roots <- function(x, y, plot_it = FALSE) pow(x, a=1/y, plot_it=plot_it)
+
